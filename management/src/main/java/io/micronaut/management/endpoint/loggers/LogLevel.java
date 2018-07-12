98c544c1c5d1469773cc7ae665b42aaa35815a0d
Matthew Moss <mossm@objectcomputing.com>
2018-07-11 19:11:39 -0500

    Add LogLevel enum

---
+++ management/src/main/java/io/micronaut/management/endpoint/loggers/LogLevel.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.management.endpoint.loggers;
+
+/**
+ * Logging levels supported by a {@link LoggingSystem}.
+ *
+ * Typically, the LoggingSystem may permit null log levels, representing an
+ * unspecified log level. For the loggers endpoint, we prefer to return the
+ * NOT_SPECIFIED pseudo-level instead of null.
+ */
+public enum LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+    OFF,
+
+    NOT_SPECIFIED,
+}
